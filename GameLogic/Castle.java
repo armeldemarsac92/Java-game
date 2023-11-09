66aad2eb5c89849dff9f9f86828bcf6e6c348ef9
CosinusTic <delmarchenathan@gmail.com>
2023-11-09 11:34:16 +0100

    feat: modified castle

--- GameLogic/Castle.java
+++ GameLogic/Castle.java
@@ -3,6 +3,12 @@ package GameLogic;
 public class Castle {
     private int hp = 500;
     private String characterSpriteFilePath;
+    private Coordinates coordinates;
+    
+
+    public Castle(){
+
+    }
 
     public int getHp(){
         return this.hp;
